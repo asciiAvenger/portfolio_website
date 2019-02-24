e0e9e7f1a291a46b281d75e021aaec43b4a1a2b6
Philipp Alber <philipp_j.alber@yahoo.com>
2019-02-24 14:01:35 +0100

    continued with about me and skills section

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,3 +6,8 @@ function toggleMenuBar() {
     menuBar.classList.toggle("menu-bar-active");
     menuButton.classList.toggle("menu-button-active");
 }
+
+let menuEntries = document.querySelectorAll("#menu-bar li");
+for (entry of menuEntries) {
+    entry.addEventListener("click", toggleMenuBar);
+}
